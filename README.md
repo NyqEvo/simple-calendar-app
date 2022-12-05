e7d2f504597d35d38134d3cf75c59d554ace9c19
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2022-12-05 15:42:28 -0500

    final changed and finished readme file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# simple-calendar-app
+# Simple Calendar App
+
+![alt text](./pictures/site-pic.png)
+
+## Description
+
+This is a functional day planner using local storage to save events and jquery to dynamically sort and list stored events. It updates the color coding on refresh, where green is future events, grey is curent events, and red is past events. It also uses dayjs to track the current time to list at the top and also compare to each div in html to properly color code. This project was made to be able to use a simple but effective planner that I would be capable of making.
+
+## Table of Contents
+
+- README file
+- index.html: the main page which is formatted as a schedule with the time at the top and dynamically changing contents and colors
+- script.js: this file contains the scripts use for the current time, event handlers, and functions to store data in local storage as well as add previous data to each section with proper coloring
+- style.css: this contains basic css including seperate classes for each time section (past, present, and future)
+- site-pic.png: a picture of the site with example plans
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var submitButtonEL = $(":button");
 var timeDisplayEl = $("#currentDay");
 if (localStorage.getItem("schedule") === null) {
@@ -9,10 +6,6 @@ if (localStorage.getItem("schedule") === null) {
     plan: ['', '', '', '', '', '', '', '']
     }))}
 var daySchedule = JSON.parse(localStorage.getItem("schedule"))
-// var daySchedule = {
-//   time: ["nine", "ten", "eleven", "twelve", "one", "two", "three", "four"],
-//   plan: ['', '', '', '', '', '', '', '']
-// };
 
 function init() {
   currentTime()
@@ -31,20 +24,20 @@ function currentTime() {
 }
 
 function checkTime() {
-  setInterval(function(){
     $('.hour').each(function() {
-      if ($(this).text() === dayjs().format('hA')) {
-        $(this).parent().removeClass("past future")
+      console.log($(this).attr("data-hour"))
+      console.log(dayjs().format('H'))
+      if ($(this).attr("data-hour") === dayjs().format('HH')) {
+        // $(this).parent().removeClass("past future")
         $(this).parent().addClass("present")
-      } else if (parseInt($(this).attr(".data-hour")) > parseInt(dayjs().format("H"))) {
-        $(this).parent().removeClass("future present")
+      } else if ($(this).attr("data-hour") < dayjs().format('HH')) {
+        // $(this).parent().removeClass("future present")
         $(this).parent().addClass("past")
       } else {
-        $(this).parent().removeClass("present past")
+        // $(this).parent().removeClass("present past")
         $(this).parent().addClass("future")
       }
     })
-  },1000)
 }
 
 $(":button").click(function(event) {
@@ -61,25 +54,3 @@ localStorage.setItem("schedule", stringSchedule);
 })
 
 init()
-
-
-// $(function () {
-//   // TODO: Add a listener for click events on the save button. This code should
-//   // use the id in the containing time-block as a key to save the user input in
-//   // local storage. HINT: What does `this` reference in the click listener
-//   // function? How can DOM traversal be used to get the "hour-x" id of the
-//   // time-block containing the button that was clicked? How might the id be
-//   // useful when saving the description in local storage?
-//   //
-//   // TODO: Add code to apply the past, present, or future class to each time
-//   // block by comparing the id to the current hour. HINTS: How can the id
-//   // attribute of each time-block be used to conditionally add or remove the
-//   // past, present, and future classes? How can Day.js be used to get the
-//   // current hour in 24-hour time?
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
-//   //
-//   // TODO: Add code to display the current date in the header of the page.
-// });
